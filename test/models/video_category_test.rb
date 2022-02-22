1ba210d2087c6fbe2baa5da4aadcf80c0c859767
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-02-22 10:58:36 +0100

    Create video_category_test.rb

---
+++ test/models/video_category_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: video_categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class VideoCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
